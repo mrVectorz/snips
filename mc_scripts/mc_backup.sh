8d2977e3b64ffd5bb7d6cf64cbc665e6fad1fffb
Marc Methot <mmethot@redhat.com>
2017-12-07 03:15:20 -0500

    fixed backups

--- mc_scripts/mc_backup.sh
+++ mc_scripts/mc_backup.sh
@@ -2,7 +2,7 @@
 
 debug=true
 
-bk_dir="${USER}/mc_backups"
+bk_dir="${HOME}/mc_backups"
 log_file="$bk_dir/log_file.log"
 if [ -d $bk_dir ]; then
   mkdir $bk_dir
@@ -22,7 +22,7 @@ decho() {
   fi
 }
 
-bk_dirs=$(ssh -vv -i ${USER}/.ssh/mc_rsa ${user}@${host} "ls ~/ | grep modpack_" 2> >(while read output; do decho $output; done >/dev/null))
+bk_dirs=$(ssh -vv -i ${HOME}/.ssh/mc_rsa ${user}@${host} "ls ~/ | grep modpack_" 2> >(while read output; do decho $output; done >/dev/null))
 decho "Current bk_dirs: $bk_dirs"
 
 for d in $bk_dirs; do
@@ -31,7 +31,7 @@ for d in $bk_dirs; do
     mkdir $bk_dir/$d
   fi
   echotee "INFO Starting rsync of $d"
-  rsync -azvrc -e "ssh -i ${USER}/.ssh/mc_rsa" ${user}@${host}:$r_home/$d/backups/ $bk_dir/$d/ | (
+  rsync -azvrc -e "ssh -i ${HOME}/.ssh/mc_rsa" ${user}@${host}:$r_home/$d/backups/ $bk_dir/$d/ | (
     while read output; do
       echotee "INFO $output"
     done

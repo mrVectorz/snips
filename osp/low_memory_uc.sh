d024941b50fb6566d765e480bd86e9697dc10b1b
Marc Methot <mmethot@redhat.com>
2018-10-23 12:09:56 -0400

    number of processes is now a variable
    This makes changing the amount of process per services easily
    changed

--- osp/low_memory_uc.sh
+++ osp/low_memory_uc.sh
@@ -3,6 +3,9 @@
 # Script to lower the memory usage on the undercloud
 # lowers all worker or wsgi process counts to 1
 
+# process count
+pcount=1
+
 services=(
  'httpd.service'
  'openstack-heat-api.service'
@@ -27,12 +30,12 @@ files=(
 
 # old style conf files
 for file in ${files[@]}; do
-  awk '{if($0 ~ /^\w+orkers *= */) print gensub(/(\w* *= *)[0-9]+$/, "\\11", "g", $0); else print}' < $file > ${file}.bak && cp -f ${file}.bak $file
+  awk -v COUNT=$pcount '{if($0 ~ /^\w+orkers *= */) print gensub(/(\w* *= *)[0-9]+$/, "\\1"COUNT, "g", $0); else print}' < $file > ${file}.bak && cp -f ${file}.bak $file
 done
 
 # wsgi files
 for file in $(grep -l -r WSGIDaemonProcess /etc/httpd/conf.d/); do
-  awk '{if ($0 ~ /WSGIDaemonProcess/) print gensub(/(.*processes=)[0-9]+(.*)/, "\\11\\2", "g", $0); else print}' < $file > ${file}.bak && mv -f ${file}.bak $file
+  awk COUNT=$pcount '{if ($0 ~ /WSGIDaemonProcess/) print gensub(/(.*processes=)[0-9]+(.*)/, "\\1"COUNT"\\2", "g", $0); else print}' < $file > ${file}.bak && mv -f ${file}.bak $file
 done
 
 # restarting services to apply the changes
@@ -44,7 +47,7 @@ for service in ${services[@]}; do
 done
 
 # validations
-echo "Everything should set to have 1 worker/process"
+echo "Everything should set to have $pcount worker/process"
 for file in ${files[@]}; do
   echo $file
   egrep '^\w+orkers *= *' $file

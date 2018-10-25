9fe3fe7b81fbb517af3e1369870df99c85b97005
Marc Methot <mmethot@redhat.com>
2018-10-25 14:05:53 -0400

    cleaned up the rc gen script

--- osp/overcloudrc_generator.sh
+++ osp/overcloudrc_generator.sh
@@ -16,27 +16,25 @@ else
   touch $rc_file
 fi
 
-function fecho() {
-  echo $* >> $rc_file
-}
-
-fecho "# Clear any old environment that may conflict."
-fecho $'for key in $( set | awk \'{FS="="}  /^OS_/ {print $1}\' ); do unset $key ; done'
-fecho export OS_NO_CACHE=True
-fecho export COMPUTE_API_VERSION=1.1
-fecho export OS_USERNAME=admin
-fecho export no_proxy=$(jq -r '.["nova::vncproxy::common::vncproxy_host"]' $conf),$(jq -r '.["keystone::endpoint::admin_url"]' $conf|cut -d: -f2 | sed 's,//,,g')
-fecho export OS_USER_DOMAIN_NAME=Default
-fecho export OS_VOLUME_API_VERSION=3
-fecho export OS_CLOUDNAME=overcloud
-fecho export OS_AUTH_URL=$(jq -r '.["heat::heat_keystone_clients_url"]' $conf)/v3
-fecho export NOVA_VERSION=1.1
-fecho export OS_IMAGE_API_VERSION=2
-fecho export OS_PASSWORD=$(jq -r '.["keystone::admin_password"]' $conf)
-fecho export OS_PROJECT_DOMAIN_NAME=Default
-fecho export OS_IDENTITY_API_VERSION=3
-fecho export OS_PROJECT_NAME=admin
-fecho export OS_AUTH_TYPE=password
-fecho export PYTHONWARNINGS="ignore:Certificate has no, ignore:A true SSLContext object is not available"
+cat << EOF > $rc_file
+# Clear any old environment that may conflict.
+for key in \$( set | awk '{FS="="}  /^OS_/ {print \$1}' ); do unset \$key ; done'
+export OS_NO_CACHE=True
+export COMPUTE_API_VERSION=1.1
+export OS_USERNAME=admin
+export no_proxy=$(jq -r '.["nova::vncproxy::common::vncproxy_host"]' $conf),$(jq -r '.["keystone::endpoint::admin_url"]' $conf|cut -d: -f2 | sed 's,//,,g')
+export OS_USER_DOMAIN_NAME=Default
+export OS_VOLUME_API_VERSION=3
+export OS_CLOUDNAME=overcloud
+export OS_AUTH_URL=$(jq -r '.["heat::heat_keystone_clients_url"]' $conf)/v3
+export NOVA_VERSION=1.1
+export OS_IMAGE_API_VERSION=2
+export OS_PASSWORD=$(jq -r '.["keystone::admin_password"]' $conf)
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_IDENTITY_API_VERSION=3
+export OS_PROJECT_NAME=admin
+export OS_AUTH_TYPE=password
+export PYTHONWARNINGS="ignore:Certificate has no, ignore:A true SSLContext object is not available"
+EOF
 
 echo "$rc_file has been generated"
